c400264a3871d03a48e3781c79f85c10f47b48e4
Mohammed Ramadan <mohammedramadan2000@gmail.com>
2024-08-02 16:00:59 +0300

    use JpaRepository instead of CRUDRepository

--- src/main/java/com/project/librarymanagementsystem/patrons/PatronRepo.java
+++ src/main/java/com/project/librarymanagementsystem/patrons/PatronRepo.java
@@ -1,8 +1,8 @@
 package com.project.librarymanagementsystem.patrons;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PatronRepo extends CrudRepository<Patron, Long> {
+public interface PatronRepo extends JpaRepository<Patron, Long> {
 }
