6a68893fd199333f5d19132309cf7c2c4560087f
Mohammed Ramadan <mohammedramadan2000@gmail.com>
2024-08-03 00:59:26 +0300

    add logging capabilities

---
+++ src/main/java/com/project/librarymanagementsystem/aop/ApiLoggingAOP.java
@@ -0,0 +1,74 @@
+package com.project.librarymanagementsystem.aop;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import java.util.Arrays;
+import java.util.Enumeration;
+
+@Slf4j
+@Aspect
+@Component
+public class ApiLoggingAOP {
+    @Before("within(com.project.librarymanagementsystem..*Controller)")
+    public void requestLog(JoinPoint joinPoint) {
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = attributes.getRequest();
+
+        log.info("API Request - Method: [{}] Path: [{}] IP: [{}]",
+                request.getMethod(),
+                request.getRequestURI(),
+                request.getRemoteAddr());
+
+        log.debug("Request Headers: {}", getHeadersAsString(request));
+        log.debug("Request Parameters: {}", request.getParameterMap());
+        log.debug("Calling method: {}.{}",
+                joinPoint.getSignature().getDeclaringTypeName(),
+                joinPoint.getSignature().getName());
+    }
+
+    private String getHeadersAsString(HttpServletRequest request) {
+        StringBuilder headers = new StringBuilder();
+        Enumeration<String> headerNames = request.getHeaderNames();
+        while (headerNames.hasMoreElements()) {
+            String headerName = headerNames.nextElement();
+            headers.append(headerName).append(": ").append(request.getHeader(headerName)).append(", ");
+        }
+        return headers.toString();
+
+    }
+
+    @AfterReturning(pointcut = "within(com.project.librarymanagementsystem..*Controller)", returning = "result")
+    public void responseLog(JoinPoint jp, Object result) {
+        if (result instanceof ResponseEntity<?> responseEntity) {
+            log.info("API Response - Status: [{}] Body: [{}]",
+                    responseEntity.getStatusCode(),
+                    responseEntity.getBody());
+        } else {
+            log.info("API Response - Body: [{}]", result);
+        }
+                log.debug("Response from method: {}.{}",
+                jp.getSignature().getDeclaringTypeName(),
+                jp.getSignature().getName());
+    }
+
+    @AfterThrowing(pointcut = "within(com.project.librarymanagementsystem..*Controller)", throwing = "ex")
+    public void logException(JoinPoint joinPoint, Exception ex) {
+        log.error("Exception in {}.{} - Exception: {} - Message: {}",
+                joinPoint.getSignature().getDeclaringTypeName(),
+                joinPoint.getSignature().getName(),
+                ex.getClass().getName(),
+                ex.getMessage());
+        log.debug("Exception stack trace:", ex);
+
+    }
+}
